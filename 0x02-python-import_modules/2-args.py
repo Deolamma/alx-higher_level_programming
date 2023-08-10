f90fa5d5142588f9707d031105226402ee73e2eb
= <=>
2023-08-10 16:23:56 +0100

    Creating a dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv_len = len(sys.argv)
+    argv_len -= 1
+    print("{} {}".format((argv_len), "argument" if (argv_len == 1)
+           else "arguments"), end="")
+    print("{}".format("." if (argv_len == 0) else ":"))
+    if argv_len != 0:
+        for i, elem in enumerate(sys.argv):
+            if i == 0:
+                continue
+            print("{}: {}".format(i, elem))
