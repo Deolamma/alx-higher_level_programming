86ce5f4b2caa1fd72fa4b4e50aa6c3c91356c350
= <=>
2023-08-14 10:30:49 +0100

    Fix: code refactor for printing elements of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i, elem in enumerate(my_list):
-        print("{}".format(my_list[i]))
+    for elem in my_list:
+        print("{}".format(elem))
--- 0x03-python-data_structures/REDME.md
+++
@@ -1,4 +0,0 @@
-# LIST & TUPLE DATA STRUCTURE IN PYTHON
-### Here we explore operations that can be carried out on the List and Tuple Data Structure in Python:
-- Indexing and Slicing
-- Duplication etc.
