f609e0b545a017257d05b3c1bb99c7bbc1194cdb
= <=>
2023-08-14 10:35:51 +0100

    Fix: proper doc. for the print int list class

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """Printing a list of integers
+    Args:
+        my_list: List to be looped through
+
+    Returns:
+        Returns nothing.
+    """
+
     for elem in my_list:
         print("{}".format(elem))
