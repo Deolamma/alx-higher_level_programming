aa29684dd4b5cf91cac4657554bfab1f19fe9488
= <=>
2023-08-14 11:10:56 +0100

    Fix: pycodestyle linting

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """Getting element at certain index of a list
-        
+
         Args:
             my_list: List to retrieve element from
             idx: index to retrieve element at
