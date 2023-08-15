d13a21b21423803e7144ace5e03729e4fd932422
= <=>
2023-08-15 05:56:11 +0100

    Printing the elements of a list in list

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Printing elments of a list in list
+        Args:
+            matrix: List in List
+
+        Returns:
+            Returns nothing.
+    """
+
+    for i, elements in enumerate(matrix):
+        for j, element in enumerate(elements):
+            print("{}".format(element), end=" ")
+        print()
