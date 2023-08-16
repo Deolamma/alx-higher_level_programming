676919a209a77f313d2d3fd6326d09bceb78ac13
= <=>
2023-08-16 18:04:13 +0100

    Get number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+
+    """Number of keys
+
+        Args:
+            a_dictionary: Dictionary to be traversed
+
+        Returns:
+            Returns the number of keys
+    """
+
+    dict_len = len(a_dictionary)
+
+    return dict_len
