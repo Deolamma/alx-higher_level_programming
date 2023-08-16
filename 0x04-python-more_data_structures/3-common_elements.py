ee96ff55f335f863f13f0abad0cafa983a26c00f
= <=>
2023-08-16 17:45:32 +0100

    Intersection between 2 sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+
+    """Getting common elements between 2 sets
+
+        Args:
+            set_1: set 1 :)
+            set_2: set 2 :)
+
+        Returns:
+            returns the common elements in both sets
+    """
+
+    return set_1 & set_2
