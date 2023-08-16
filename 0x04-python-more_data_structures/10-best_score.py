9c3a94e1feaad777874d535c4d75dc0e760ada83
= <=>
2023-08-16 23:10:52 +0100

    Returning the key with highest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+
+    """Returning key with highest score
+
+        Args:
+            a_dictionary: Dictionary to be traversed
+
+        Returns:
+            Returns the key with the highest value
+    """
+
+    if a_dictionary is None:
+        return None
+
+    best_score = 0
+    best_score_key = ""
+
+    for k, v in a_dictionary.items():
+        if a_dictionary[k] > best_score:
+            best_score = a_dictionary[k]
+            best_score_key = k
+    return best_score_key
