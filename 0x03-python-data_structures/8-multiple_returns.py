f2986a43d8e1b72f41ccd1a2924cdd35ad1f0318
= <=>
2023-08-16 04:36:18 +0100

    Returning a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+def multiple_returns(sentence):
+    """Making multiple returns
+        Args:
+            sentence: String to be indexed
+
+        Returns:
+            Returns a tuple: sentence Length and first char of sentence
+    """
+
+    sentence_len = len(sentence)
+    if sentence == "":
+        return sentence_len, None
+    return sentence_len, sentence[0]
