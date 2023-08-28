6e5928ac08ec7f82cd73e3630be5b024eaa238a4
Deolamma <=>
2023-08-28 19:02:52 +0100

    Raising IndexError while printing valid integers from a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """Printing Valid integers in a List
+
+        Args:
+            my_list: List to be treversed
+            x: number of elements to be printed out
+
+        Return:
+            returns the number of valid integer printed
+    """
+    count, list_len = 0, 0
+    
+    for value in my_list:
+        list_len += 1
+    for i in range(x):
+        try:
+            int(my_list[i])
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, OverflowError, TypeError):
+            pass
+        except IndexError:
+            raise
+    print()
+    return count
