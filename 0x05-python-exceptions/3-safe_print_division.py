a2fe9912c32636f9721a5733a0c5768b3cdff739
Deolamma <=>
2023-08-28 21:44:16 +0100

    Handling ZeroDivisioError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,5 @@
 #!usr/bin/python3
 def safe_print_division(a, b):
-    """Dividing Safely
-
-        Args:
-            a: first integer
-            b: second integer
-
-        Return:
-            Returns the result of the division OR None if it fails
-    """
     try:
         result = a / b
     except (FloatingPointError, ZeroDivisionError, TypeError):
