5264fff9df02f9559d008e1e0e0061139bc12dd3
Deolamma <=>
2023-08-28 23:02:46 +0100

    Raising Exceptions

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Raise an exception with a message
+
+        Args:
+            message: Message to be printed when exception is raised
+
+        Returns:
+            NOTHING
+    """
+    raise NameError(message);
