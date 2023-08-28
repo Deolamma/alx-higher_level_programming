34d826c8264272374486850940ee90a4ca2c09f6
Deolamma <=>
2023-08-28 15:16:05 +0100

    Catching NameError and IndexError

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Safe Printing A Listing
+        
+        Args:
+            my_list: List to be printed out
+            x: number of elements to print out
+
+        Return:
+            Returns nothing
+    """
+    count = 0
+    for value in my_list[:x]:
+        try:
+            int(value)
+            print("{:d}".format(value), end="")
+            count += 1
+        except (ValueError, IndexError):
+            pass
+    print()
+    return count
