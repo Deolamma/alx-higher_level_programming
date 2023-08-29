cc6fb50a7eeba998001e2bc7b12f67ee3fdbc972
Deolamma <=>
2023-08-29 20:45:20 +0100

    Implementing setters and getters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+This is a class with a private instance attribute
+"""
+
+
+class Square:
+
+    """Creates a class with a private instance attribute
+        Note:
+            size is private.
+        Attributes:
+            size: Private instance of the class
+                  It receives the size of the square passed by the object
+    """
+
+    def __init__(self, size=0):
+
+        """This is where instantiation takes place
+
+            Args:
+                size: size of square passed by object of class
+                      if None, size defaults to 0
+            Raises:
+                ValueError: This is raised if size is less than zero
+                TypeError: Raises error if size is not an integer
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of a square
+
+            Setter Args:
+                value: Size of square to be set
+
+            Returns:
+                Returns the size of a square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of a square
+
+            Args:
+                value: Val to be used to update the size of square
+
+            Returns:
+                Returns Nothing
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of a square
+
+            Args:
+                NIL
+
+            Returns:
+                Returns the area of a square
+        """
+
+        area_of_square = self.__size * self.__size
+
+        return area_of_square
