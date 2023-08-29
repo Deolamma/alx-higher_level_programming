d85f405a7f629594957718ccc60ce48092133bc6
Deolamma <=>
2023-08-29 17:08:02 +0100

    Creating an empty class with proper doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,11 @@
 class Square:
     """Defining an Empty class Square
         This class defines an empty Square class
-        It does nothing and has no attributes
+
+        Attributes:
+            NIL
+
+        Todo:
+            It does nothing and has no attributes
     """
+    pass
