fe41f503e444a8006b8b0b537f5a4a09891d023f
Deolamma <=>
2023-08-30 00:06:39 +0100

    Fix: Instantiating a privat instace of a class properly using setters and getters

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,8 +28,8 @@ class Square:
                 size: size of square passed by object of class
                       if None, size defaults to 0
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def position(self):
@@ -50,8 +50,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if ((type(value) is not tuple) and (len(value) != 2) and
-                (value[0] < 0) and (value[1] < 0)):
+        if (
+            (type(value) is not tuple) or (len(value) != 2) or
+            (value[0] < 0) or (value[1] < 0) or (type(value[0]) is not int)
+            or (type(value[1]) is not int)
+           ):
+            print("I got here _______________________")
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
