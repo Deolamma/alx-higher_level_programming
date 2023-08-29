2bbe25b60c265c31cbae5873fab92e71e0ccc05d
Deolamma <=>
2023-08-29 21:08:23 +0100

    Fix: Error message text

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,7 +22,7 @@ class Square:
                 size: size of square passed by object of class
                       if None, size defaults to 0
         """
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
@@ -44,7 +44,7 @@ class Square:
     @size.setter
     def size(self, value):
         if type(value) is not int:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
