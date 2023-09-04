005b055d56285215338d6c58a07e1dfc9a393473
Deolamma <=>
2023-09-04 22:09:23 +0100

    Creating an empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a simple rectangle class that does nothing
+"""
+
+class Rectangle:
+    """
+        Rectangle class
+        
+        No properties or methods
+
+        No args
+    """
+    pass
