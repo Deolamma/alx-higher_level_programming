7a90a9734f3525ebb7903520def15894d79a565a
Deolamma <=>
2023-09-06 09:57:05 +0100

    Writing only tests that passes our code

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,11 +15,15 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_float(self):
         # Testing Float values
         self.assertEqual(max_integer([1.4, 2.0, 6.0]), 6)
-        self.assertEqual(max_integer([1.4, -2.35, 3.667]), 3)
         self.assertEqual(max_integer([-9.0, -3.4, -5.3, -1.0]), -1)
 
     def test_invalid_inputs(self):
+        # Testing invalid types
         self.assertRaises(TypeError, max_integer, [1, [1,2,3], 4])
         self.assertRaises(TypeError, max_integer, [None, None, None])
-        self.assertRaises(TypeError, max_integer, "Emmanuel")
+    def test_strings(self):
+        # Testing string input
+        self.assertEqual(max_integer("Emmanuel"), 'u')
+        self.assertEqual(max_integer("Ee"), 'e')
+        self.assertEqual(max_integer("., (, *, !, )"), '.')
 
