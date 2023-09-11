7b54721fd51ad29dcfbde56a10a2ceecb4a08980
Deolamma <=>
2023-09-11 22:51:42 +0100

    comapares 2 objects

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module checks if an object is an instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if two args are the same object
+
+    Args:
+        obj: Any Object
+        a_class: Any class
+
+    Return:
+        True if obj is an instance of a_class.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
