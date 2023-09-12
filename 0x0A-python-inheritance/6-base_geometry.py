bb57d223ac6bc4e7540894a2c9b651749d4a42ab
Deolamma <=>
2023-09-12 02:28:08 +0100

    creating a public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Creating a Base Geometry class
+"""
+
+
+class BaseGeometry:
+    """
+        A simple class
+    """
+    def area(self):
+        """
+        A simple method
+
+        Args:
+            NIL
+        Raises:
+            Exception: when area is called.
+        Return:
+            NIL
+        """
+
+        raise Exception("area() is not implemented")
