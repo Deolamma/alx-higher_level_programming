f21cf09bcac5fa58234d3b9f5bc443148826d3bf
Deolamma <=>
2023-09-12 03:58:30 +0100

    Inheriting from another class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A simple class that inherits from BaseGeometry class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Inherits from the BaseGeometry class
+    """
+
+    def __init__(self, width, height):
+        """
+        Instatiation method for Rectangle class
+        Args are to be validated by integr_validator method of BaseGeometry
+
+        Args:
+            width: width of Rectangle
+            height: height of Rectangle
+
+        Returns:
+            NIL
+        """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height ", height)
+        self.__width = width
+        self.__height = height
