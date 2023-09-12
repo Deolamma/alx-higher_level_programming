769e1188e0b4c1c36039024d74a592eade1fec2c
Deolamma <=>
2023-09-12 16:18:38 +0100

    Update: documentation of module

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module reads content of a file and prints to STDOUT
+"""
+
+
 def read_file(filename=""):
     """Read File
     Reads a file and prints to stdout
@@ -12,6 +17,7 @@ def read_file(filename=""):
     Return:
         NIL
     """
+
     if type(filename) is not str:
         raise TypeError("Filename must be a String")
     with open(filename, "r", encoding="utf-8") as open_f:
