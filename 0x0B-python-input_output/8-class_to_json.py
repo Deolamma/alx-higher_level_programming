6b7471d8d6b965ce5b3cada5b557b2564a4d2b37
Deolamma <=>
2023-09-12 21:30:50 +0100

    Returning a dictionary rep. of class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+"""
+This module returns the dictionary rep. of a class
+"""
+
+
+def class_to_json(obj):
+    """Class To JSON
+    This module returns the dictionary rep. of an object
+    This is then used for JSON serialization
+
+    Args:
+        obj; Instance of an object
+
+    Return:
+        returns the dictionary of a class
+    """
+
+    return obj.__dict__
