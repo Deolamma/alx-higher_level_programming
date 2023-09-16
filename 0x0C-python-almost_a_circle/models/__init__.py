db0a8c98272d90f6732727e8c884fcbc4f6f182a
Deolamma <=>
2023-09-16 04:02:57 +0100

    Package declarator and initializer

---
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+"""
+This is a Models Package
+
+In this package I explore the concepts of Inheritance, Encapsulation
+Data hiding, JSON Serialization an Deserialization etc.
+"""
