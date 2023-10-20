7270754f517ba9fc144e31742336da3b9bec0261
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-20 18:26:12 +0100

    update: specifying primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creating a primmary key
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-
+USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS states(
 	id INT AUTO_INCREMENT UNIQUE NOT NULL, 
 	name VARCHAR(256) NOT NULL,
