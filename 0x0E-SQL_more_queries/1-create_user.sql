c43d646086ff37db35ccf6a5422af9c6cf55690a
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-20 17:20:27 +0100

    Creating a root user with all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creating a root user on a database
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED WITH caching_sha2_plugin BY "user_0d_1_pwd";
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost WITH GRANT;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED WITH 'caching_sha2_plugin' BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
