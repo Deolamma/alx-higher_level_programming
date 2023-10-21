2ef44630557337f4cbdcc6155f29e6eb05ba7ad9
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-21 10:56:57 +0100

    Update: aliasing of output column

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Selecting and counting occurrences of a record in another table
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
