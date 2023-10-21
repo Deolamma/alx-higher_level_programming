2a5131d4f7afb2e8e82249823b8c15d3d41bb9ce
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-21 10:54:18 +0100

    Perfrom SELECT, COUNT, GROUP BY, and ORDER BY on records

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Selecting and counting occurrences of a record in another table
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
