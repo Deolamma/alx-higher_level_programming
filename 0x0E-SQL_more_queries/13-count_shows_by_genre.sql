b5956ffe5679cf06051676647ea24b86347487d8
Deolamma <adebanjoemmanuel01@gmail.com>
2023-10-21 11:17:00 +0100

    Update: added condition to make sure genres not matched withany show is not added

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- Selecting and counting occurrences of a record in another table
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.genre_id > 0 OR tv_show_genres.genre_id IS NOT NULL
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
