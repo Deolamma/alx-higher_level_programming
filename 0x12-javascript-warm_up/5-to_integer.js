cd0672fa8351709ba358822fe89df3a600e3a855
deolamma <adebanjoemmanuel01@gmail.com>
2023-11-14 10:10:12 +0100

    Fix: Semistandard linting for JS

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const cmdLineArg = process.argv[2];
 
 if (isNaN(cmdLineArg)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number:', parseInt(cmdLineArg));
+  console.log('My number:', parseInt(cmdLineArg));
 }
