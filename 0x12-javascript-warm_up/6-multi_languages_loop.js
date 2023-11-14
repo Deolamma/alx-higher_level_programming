b4f6d8001b8a2498316ba8e96bd0893ee3624c23
deolamma <adebanjoemmanuel01@gmail.com>
2023-11-14 10:31:48 +0100

    Semistandard linting for JS

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const myArr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 myArr.forEach((element) => {
-	console.log(element);
+  console.log(element);
 });
