2cdbf605249414203c1bb466cdf0fd40f42440de
deolamma <adebanjoemmanuel01@gmail.com>
2023-11-15 16:43:17 +0100

    reversing a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let newList = [];
+  // check if list length is greater than zero
+  if (list && (list.length > 0)) {
+    // newListIndexNum helps us avoid overflow when reordering list
+    let newListIndexNum = list.length - 1;
+    list.forEach((item) => {
+      newList[newListIndexNum] = item;
+      newListIndexNum--;
+    });
+  } else {
+    // Return the list if it is empty or null
+    return list;
+  }
+  return newList;
+};
