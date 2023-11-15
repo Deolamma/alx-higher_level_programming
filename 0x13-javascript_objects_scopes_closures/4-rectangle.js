e60cac60b9620aedc6c2854ebf1f6f5dc232392c
deolamma <adebanjoemmanuel01@gmail.com>
2023-11-15 01:59:13 +0100

    more methods in the rectangle class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,16 +18,16 @@ class Rectangle {
       console.log();
     }
   }
-  
+
   rotate () {
-     let temp = this.width;
-     this.width = this.height;
-     this.height = temp;
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
-  
+
   double () {
-     this.width *= 2;
-     this.height *= 2;
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 module.exports = Rectangle;
